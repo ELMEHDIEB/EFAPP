fdb241dddc1650b41003a23fe43210ec4d659059
EL MEHDI <ELMEHDIBENC@GMAIL.COM>
2026-06-25 03:09:20 +0100

    Phase 4 complete

--- electron/preload.js
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  platform: process.platform,
-  version: process.versions.electron,
-  saveBackup: (jsonStr) => ipcRenderer.invoke('save-backup', jsonStr),
-  showNotification: (title, body) => ipcRenderer.send('show-notification', title, body),
-});
